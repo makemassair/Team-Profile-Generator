148a0f5511908a2b9b5c12a86bc1bd8308124acf
Michael Samaras <mike@makemassair.co.uk>
2023-02-24 19:14:41 +0000

    Employee tests passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
@@ -16,4 +17,9 @@ test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
+});
+
+test("The engineer should extend the employee class", () => {
+  const engineer = new Engineer();
+  expect(engineer).toBeInstanceOf(Employee);
 });
