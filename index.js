f7b2ca3da3400281488dab357c1d9f4fa5b706f2
Michael Samaras <mike@makemassair.co.uk>
2023-02-26 15:52:13 +0000

    Removed email-validator from code + dependancies

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Intern = require("./lib/Intern");
 const inquirer = require('inquirer');
 const path = require("path");
 const fs = require("fs");
-var validator = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -204,8 +203,6 @@ function createDocument() {
         return `Your file has been created in the folder: output`;
     }
 }
-
-
 
 assembleTeam();
 
